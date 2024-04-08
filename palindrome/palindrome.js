152cc75113753837fd9206951610bb80881b2c79
Francois Disubi <francois@kinshasadigital.com>
2024-04-08 13:01:02 +0100

    add palindrome

--- palindrome/palindrome.js
+++ palindrome/palindrome.js
@@ -1,7 +1,9 @@
-// 3. Écrire une fonction qui vérifie si une chaîne de caractères est un palindrome.
+// Écrire une fonction qui vérifie si une chaîne de caractères est un palindrome.
 
 
-
+function removeSpecialCharactersAndSpaces(str) {
+    return str.replace(/[\s`~!@#$%^&*()_|+\-=?;:'",.<>\{\}\[\]\\\/]/gi, '');
+}
 
 function isPalindrome(str) {
 
