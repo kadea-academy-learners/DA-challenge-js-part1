17ad5166a60abf39742033781271f4c1c97a1384
Francois Disubi <francois@kinshasadigital.com>
2024-04-08 15:56:25 +0100

    fix squareRoot challenge

--- squareRoot/squareRoot.js
+++ squareRoot/squareRoot.js
@@ -2,19 +2,7 @@
 
 
 function squareRoot(number) {
-    if (number === 0) return 0;
-    if (number < 0) return NaN;
 
-    let x = number;
-    let y = (x + number / x) / 2; // Première estimation
-
-    // Répéter jusqu'à la convergence
-    while (Math.abs(y - x) >= 1e-10) {
-        x = y;
-        y = (x + number / x) / 2;
-    }
-
-    return y;
 }
 
 module.exports = squareRoot;
