a1f6b159d5906ec8b853330bf2f8ee636b4dccf8
ChrisMwanya <cmwanya@gmail.com>
2024-04-08 18:06:29 +0100

    Refactor fizzBuzz function to include comments and improve readability

--- fizzBuzz/fizzBuzz.js
+++ fizzBuzz/fizzBuzz.js
@@ -1,8 +1,10 @@
-// Écrivez une fonction prenant un paramètre N. Cette fonction doit retourner une chaîne de caractères contenant tous les nombres de 1 à N. Cependant, pour les multiples de 3, remplacez chaque nombre par "Fizz". Pour les multiples de 5, remplacez chaque nombre par "Buzz". Enfin, pour les nombres qui sont à la fois des multiples de 3 et de 5, remplacez-les par "FizzBuzz".
+/* Écrivez une fonction prenant un paramètre N.
+ Cette fonction doit retourner une chaîne de caractères contenant tous les nombres de 1 à N.
+ Cependant, pour les multiples de 3, remplacez chaque nombre par "Fizz".
+ Pour les multiples de 5, remplacez chaque nombre par "Buzz".
+ Enfin, pour les nombres qui sont à la fois des multiples de 3 et de 5, remplacez-les par "FizzBuzz".
 
-
-function fizzBuzz(n) {
-
-}
+*/
+function fizzBuzz(n) {}
 
 module.exports = fizzBuzz;
